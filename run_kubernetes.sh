148a436f7c27c98418a0be21c61bbcd458d9e269
Melkamu273 <melkamutessema372@gmail.com>
2022-07-29 15:11:01 +0300

    add out put text

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=genetadno/housing-price-predictor
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run predictor-app --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/predictor-app 8000:80
